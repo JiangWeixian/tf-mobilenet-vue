678f06e60c44fa9bbc757a4c182e1c2f1be04818
JiangWeixian <saber_wei@163.com>
2018-05-14 14:12:16 +0800

    webpack+vue get it!

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <img src="./assets/logo.png" alt="logo">
+    Welcome Webpack
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    data() {
+      return {
+        name: 'App'
+      }
+    }
+  }
+</script>
+
+<style>
+  .app {
+    font-size: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
