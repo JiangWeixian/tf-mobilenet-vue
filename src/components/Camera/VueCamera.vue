26a85473a322c2a60a4f93db8071b4b4503040d2
JiangWeixian <saber_wei@163.com>
2018-05-15 09:26:01 +0800

    update

--- src/components/Camera/VueCamera.vue
+++ src/components/Camera/VueCamera.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="vue-camera">
-    this is camera
+    <video></video>
   </div>
 </template>
 
@@ -10,6 +10,6 @@
   }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+@import "VueCamera.styl"
 </style>
